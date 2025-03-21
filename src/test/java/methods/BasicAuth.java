ebf0b363ee2e432035d17d5bc50c336bee506e59
ShonAntony <shon.antony99@gmail.com>
2025-03-21 17:21:56 +0530

    Just adding the basic auth script

---
+++ src/test/java/methods/BasicAuth.java
@@ -0,0 +1,22 @@
+package methods;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+public class BasicAuth {
+	
+	@Test
+	public void basicAuth() {
+		Response response = RestAssured.given().auth().basic("rmgyantra","rmgy@9999").when().get("http://49.249.29.6:8084/login");
+		String stringRespose = response.asString();
+		JsonPath jsonPath = new JsonPath(stringRespose);
+		String jsonPathString = jsonPath.getString("jwtToken");
+		System.out.println("JwtToken :"+jsonPathString);
+		
+	}
+
+}
